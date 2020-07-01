5bf2a30ea7be93ab2626f4652dd500f6cd628e59
DESKTOP-VL3ISR6\Owner <wfd0000@gmail.com>
2020-07-01 15:19:04 +0900

    ファイル追加

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import "./styles.css";
+
+const canvas = document.querySelector("#draw-area");
+const context = canvas.getContext("2d");
+
+canvas.addEventListener("mousemove", event => {
+  draw(event.layerX, event.layerY);
+});
+canvas.addEventListener("touchmove", event => {
+  draw(event.layerX, event.layerY);
+});
+
+canvas.addEventListener("mousedown", () => {
+  context.beginPath();
+  isDrag = true;
+});
+canvas.addEventListener("mouseup", () => {
+  context.closePath();
+  isDrag = false;
+});
+canvas.addEventListener("touchstart", () => {
+  context.beginPath();
+  isDrag = true;
+});
+canvas.addEventListener("touchend", () => {
+  context.closePath();
+  isDrag = false;
+});
+
+const clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+let isDrag = false;
+function draw(x, y) {
+  if (!isDrag) {
+    return;
+  }
+
+  context.strokeStyle = "rgb(0, 233, 255)";
+  context.lineWidth = 5;
+  context.lineTo(x, y);
+  context.stroke();
+}
+
+// document.getElementById("app").innerHTML = `
+// <h1>Hello Vanilla!</h1>
+// <div>
+//   We use the same configuration as Parcel to bundle this sandbox, you can find more
+//   info about Parcel
+//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+// </div>
+// `;
